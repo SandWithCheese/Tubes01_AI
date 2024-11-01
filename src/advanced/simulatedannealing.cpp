90c6d7f0d5ff6b93289fcad65ca7a379dcc8e496
bagassambega <bagassambega@gmail.com>
2024-11-02 00:50:33 +0700

    feat: simulated annealing

--- src/advanced/simulatedannealing.cpp
+++ src/advanced/simulatedannealing.cpp
@@ -1,20 +1,72 @@
 #include "simulatedannealing.hpp"
 
-SimulatedAnnealing::SimulatedAnnealing() : Solver() {
-    this->temperature = 1000;
-    this->cooling_rate = 0.003;
-    this->iterations = 0;
-}
-
 SimulatedAnnealing::SimulatedAnnealing(float temperature, float cooling_rate) : Solver() {
     this->temperature = temperature;
     this->cooling_rate = cooling_rate;
-    this->iterations = 0;
+    this->cube = MagicFive();
+    this->acceptance_probability = 0.5;
 }
 
-SimulatedAnnealing::SimulatedAnnealing(const MagicFive& other) : Solver(other) {
+SimulatedAnnealing::SimulatedAnnealing(float temperature, float cooling_rate, float acceptance_probability) : Solver() {
+    this->temperature = temperature;
+    this->cooling_rate = cooling_rate;
+    this->cube = MagicFive();
+    this->acceptance_probability = acceptance_probability;
+}
+
+SimulatedAnnealing::SimulatedAnnealing(const MagicFive& other, float temperature, float cooling_rate) : Solver(other) {
+    this->temperature = temperature;
+    this->cooling_rate = cooling_rate;
     this->cube = other;
-    this->temperature = 1000;
-    this->cooling_rate = 0.003;
-    this->iterations = 0;
+    this->acceptance_probability = 0.5;
+}
+
+SimulatedAnnealing::SimulatedAnnealing(const MagicFive& other, float temperature, float cooling_rate, float acceptance_probability) : Solver(other) {
+    this->temperature = temperature;
+    this->cooling_rate = cooling_rate;
+    this->cube = other;
+    this->acceptance_probability = acceptance_probability;
+}
+
+vector<vector<int>> SimulatedAnnealing::generateRandomSuccessor() {
+    vector<vector<int>> random_successor = cube.getData();
+    int i, j, k, l;
+    do {
+        i = rand() % cube.getRows();
+        j = rand() % cube.getCols();
+        k = rand() % cube.getRows();
+        l = rand() % cube.getCols();
+    } while (i == k && j == l);
+    swap(random_successor[i][j], random_successor[k][l]);
+    return random_successor;
+}
+
+float SimulatedAnnealing::acceptanceProbability(int objCurrent, int objNeighbor) {
+    return exp((float)(objNeighbor - objCurrent) / temperature);
+}
+
+void SimulatedAnnealing::decreaseTemperature() {
+    temperature = temperature  - cooling_rate; // TODO define temperature decrease functoin
+}
+
+void SimulatedAnnealing::solve() {
+    int objCurrent = cube.objectiveFunction();
+    while (temperature > 0 && objCurrent < 0) {
+        vector<vector<int>> neighbor = generateRandomSuccessor();
+        int objNeighbor = MagicFive(neighbor).objectiveFunction();
+        if (objNeighbor > objCurrent) {
+            cube = MagicFive(neighbor);
+            objCurrent = objNeighbor;
+        } else if (acceptanceProbability(objCurrent, objNeighbor) > (float)rand() / RAND_MAX) {
+            cube = MagicFive(neighbor);
+            objCurrent = objNeighbor;
+        }
+        decreaseTemperature();
+    }
+    cout << "Objective function: " << objCurrent << endl;
+    if (objCurrent == 0) {
+        cout << "Solution found." << endl;
+    } else {
+        cout << "Solution not found." << endl;
+    }
 }
--- src/advanced/simulatedannealing.hpp
+++ src/advanced/simulatedannealing.hpp
@@ -14,12 +14,17 @@ class SimulatedAnnealing : public Solver {
 private:
     float temperature;
     float cooling_rate;
-    int iterations;
+    float acceptance_probability;
 
 public:
-    SimulatedAnnealing();
     SimulatedAnnealing(float temperature, float cooling_rate);
-    SimulatedAnnealing(const MagicFive& other);
+    SimulatedAnnealing(float temperature, float cooling_rate, float acceptance_probability);
+    SimulatedAnnealing(const MagicFive& other, float temperature, float cooling_rate);
+    SimulatedAnnealing(const MagicFive& other, float temperature, float cooling_rate, float acceptance_probability);
+    vector<vector<int>> generateRandomSuccessor();
+    float acceptanceProbability(int objCurrent, int objNeighbor);
+    void decreaseTemperature();
+    void solve() override;
 };
 
 #endif
