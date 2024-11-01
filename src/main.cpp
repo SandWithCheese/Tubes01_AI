559a601bc43b9e47fea29b89b01f883765c7f21c
bagassambega <bagassambega@gmail.com>
2024-11-02 00:50:42 +0700

    fix: add all method to main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "hillclimb/stochastic.hpp"
 #include "hillclimb/randomrestart.hpp"
 #include "advanced/genetic.hpp"
+#include "hillclimb/steepestascent.hpp"
+#include "hillclimb/sidewaysmove.hpp"
+#include "advanced/simulatedannealing.hpp"
+#include "advanced/genetic.hpp"
 
 using namespace std;
 
@@ -10,8 +14,60 @@ int main() {
     MagicFive cube;
     cube.showCube();
 
-    Solver* solver = new RandomRestart(cube, 100000);
-    solver->solve();
-    solver->showCube();
+    int choice = 0;
+    while (choice < 1 || choice > 6) {
+        try {
+            cout << "Choose the algorithm to solve the Magic Five Cube:" << endl;
+            cout << "1. Hill Climbing: Steepest Ascent" << endl;
+            cout << "2. Hill Climbing: Sideways Move" << endl;
+            cout << "3. Hill Climbing: Random Restart" << endl;
+            cout << "4. Hill Climbing: Stochastic" << endl;
+            cout << "5. Simulated Annealing" << endl;
+            cout << "6. Genetic Algorithm" << endl;
+            cout << "Choice: ";
+            cin >> choice;
+        } catch (const exception& e) {
+            cout << "Invalid input. Please try again." << endl;
+        }
+    }
+
+    switch (choice) {
+        case 1: {
+            SteepestAscent sa = SteepestAscent(cube);
+            sa.solve();
+            break;
+        }
+        case 2: {
+            SidewaysMove sm = SidewaysMove(cube, 1000); // Dummy value
+//            sm.solve(); // Immplement
+            break;
+        }
+        case 3: {
+            RandomRestart rr = RandomRestart(cube, 1000); // Dummy value
+            rr.solve();
+            break;
+        }
+        case 4: {
+            Stochastic st = Stochastic(cube);
+            st.solve();
+            break;
+        }
+        case 5: {
+            SimulatedAnnealing sa = SimulatedAnnealing(cube, 1000, 0.001, 0.5); // Dummy value
+            sa.solve();
+            break;
+        }
+        case 6: {
+            Genetic gen = Genetic(cube); // Dummy value
+            gen.solve();
+            break;
+        }
+        default : {
+            Stochastic st = Stochastic(cube);
+            st.solve();
+            break;
+        }
+
+    }
     return 0;
 }
