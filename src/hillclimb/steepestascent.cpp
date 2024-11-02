2eb052a8fc71807bdfe7a8ffbaff38b471a540e4
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-11-02 14:37:30 +0700

    feat: add output matrix n objective

--- src/hillclimb/steepestascent.cpp
+++ src/hillclimb/steepestascent.cpp
@@ -1,4 +1,5 @@
 #include "steepestascent.hpp"
+#include <iostream> 
 
 SteepestAscent::SteepestAscent() : HillClimb() {}
 SteepestAscent::SteepestAscent(const MagicFive& other) : HillClimb(other) {}
@@ -15,7 +16,7 @@ void SteepestAscent::solve() {
         int bestScore = currentScore;
 
         for (const auto& successor : successors) {
-            cube.setData(MagicFive::listToMatrix(successor));  // bangkitkan successor cube
+            cube.setData(MagicFive::listToMatrix(successor));  // generate suksesor
             int newScore = cube.objectiveFunction();
 
             if (newScore > bestScore) {
@@ -26,9 +27,28 @@ void SteepestAscent::solve() {
         }
 
         if (improved) {
-            cube.setData(MagicFive::listToMatrix(bestSuccessorData));  // set best successor
+            cube.setData(MagicFive::listToMatrix(bestSuccessorData));  // ambil suksesor best value 
             currentScore = bestScore;
         }
 
-    } while (improved);  // lanjut hingga tidak ada successor yang skornya lebih baik
+    } while (improved);  // lanjut kalo ada tetangga yang better
+
+    // result objective function 
+    std::cout << "Objective function: " << currentScore << std::endl;
+
+    // final cube
+    std::cout << "Solution matrix:" << std::endl;
+    for (const auto& row : cube.getData()) {
+        for (int element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // optimal global or not
+    if (currentScore == 0) {
+        std::cout << "Optimal solution found." << std::endl;
+    } else {
+        std::cout << "Optimal solution not found." << std::endl;
+    }
 }
